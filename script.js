003ab6a3c4daf2fc299587144447667f0cb7cd3d
werikrocha-cmyk <werik.rocha@escola.pr.gov.br>
2026-09-04 10:39:34 -0300

    n

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ curtiu = true;
 
  function mudaTema() {
   const corpoPagina = document. body;
-  if(corpoPagina.classList.contains("tema escuro")) {
+  if(corpoPagina.classList.contains("tema-escuro")) {
     corpoPagina.classList.remove("tema-escuro");
   } else{
     corpoPagina.classList.add("tema-escuro");
